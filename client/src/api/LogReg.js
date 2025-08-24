d8c38f04605350f13ce9a0ac04cee8967a35d836
Sythe <sythe@gmail.com>
2025-08-24 17:53:09 +0530

    IP mistake

--- client/src/api/LogReg.js
+++ client/src/api/LogReg.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const IP = window.location.hostname;
 
 const LogRegApi = axios.create({
-  // baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  baseURL: `http://${IP}:5000/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  // baseURL: `http://${IP}:5000/api`,
 });
 export default LogRegApi;
